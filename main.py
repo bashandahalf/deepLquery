1c68bcf7bfaae0b6bbef3b4d3611f618b88b94d9
bashandahalf <bashandahalf@og8.org>
2021-01-18 21:23:11 +0200

    class based to allow multi query

--- main.py
+++ main.py
@@ -3,46 +3,90 @@ import requests
 import time
 import random
 import json
+import logging
+import sys
 from user_agent import generate_user_agent
 
 
-def queryDeepL(query="bubblegum"):
-    session = requests.Session()
-    session.headers['User-Agent'] = generate_user_agent()
+logger = logging.getLogger()
 
-    r = session.get("https://www.deepl.com/translator")
-    if r.status_code != 200:
-        print('error calling deepl.com')
-        return
-    r = session.get("https://static.deepl.com/css/deepl.$b89835.css", headers={'referer': "https://www.deepl.com/translator"})
-    if r.status_code != 200:
-        print('error getting css 1')
-        return
-    r = session.get("https://static.deepl.com/css/cookieBanner.$21aa7c.css", headers={'referer': "https://www.deepl.com/translator"})
-    if r.status_code != 200:
-        print('error getting css 2')
-        return
 
-    if not session.cookies.keys():
-        print('didnt get cookie')
-        return
 
-    timestamp = int(time.time()*100)
-    id = 1000 * int(random.random()*1000) + 2
+class DeepLQuery:
 
-    jj = {"jsonrpc":"2.0","method": "LMT_handle_jobs","params":{"jobs":[{"kind":"default","raw_en_sentence":query,"raw_en_context_before":[],"raw_en_context_after":[],"preferred_num_beams":len(query),"quality":"fast"}],"lang":{"user_preferred_langs":["DE","EN"],"source_lang_user_selected":"auto","target_lang":"DE"},"priority":-1,"commonJobParams":{},"timestamp":timestamp},"id":id}
 
-    r = session.post("https://www2.deepl.com/jsonrpc", json=jj)
-    if r.status_code != 200:
-        print('error getting main query')
-        return
+    def __init__(self):
 
-    response = json.loads(r.content)
+        self.id = 1000 * int(random.random()*1000) + 1
+        self.session = requests.Session()
+        self.session.headers['User-Agent'] = generate_user_agent()
 
-    for beam in response['result']['translations'][0]['beams']:
-        print(beam['postprocessed_sentence'])
+        r = self.session.get("https://www.deepl.com/translator")
+        if r.status_code != 200:
+            logger.error('error calling deepl.com')
+            return
+        r = self.session.get("https://static.deepl.com/css/deepl.$b89835.css", headers={'referer': "https://www.deepl.com/translator"})
+        if r.status_code != 200:
+            logger.error('error getting css 1')
+            return
+        r = self.session.get("https://static.deepl.com/css/cookieBanner.$21aa7c.css", headers={'referer': "https://www.deepl.com/translator"})
+        if r.status_code != 200:
+            logger.error('error getting css 2')
+            return
+        if not self.session.cookies.keys():
+            logger.error('didnt get cookie')
+            return
+
+
+    def query(self, q):
+
+        self.id += 1
+        timestamp = int(time.time()*100)
+
+        jj = {
+            "jsonrpc": "2.0",
+            "method": "LMT_handle_jobs",
+            "params": {
+                "jobs": [{
+                    "kind": "default",
+                    "raw_en_sentence": q,
+                    "raw_en_context_before": [],
+                    "raw_en_context_after": [],
+                    "preferred_num_beams": len(q),
+                    "quality": "fast"
+                }],
+                "lang": {
+                    "user_preferred_langs": ["DE","EN"],
+                    "source_lang_user_selected": "auto",
+                    "target_lang": "DE"
+                },
+                "priority": -1,
+                "commonJobParams": {},
+                "timestamp": timestamp
+            },
+            "id": self.id
+        }
+
+        r = self.session.post("https://www2.deepl.com/jsonrpc", json=jj)
+        if r.status_code != 200:
+            logger.error('error getting main query')
+            logger.error(f'r.status_code: {r.status_code}')
+            logger.error(f'r.content: {r.content}')
+            logger.error(f'id: {self.id}')
+            return
+
+        response = json.loads(r.content)
+
+        for beam in response['result']['translations'][0]['beams']:
+            return beam['postprocessed_sentence']
+
 
 
 if __name__ == '__main__':
-    queryDeepL()
+
+    dlq = DeepLQuery()
+
+    while True:
+        inp = sys.stdin.readline().strip()
+        print(dlq.query(inp))
 
