d4693a4d041a8f1baf0d4b35dd7b7f58139c00e9
bashandahalf <bashandahalf@og8.org>
2021-01-22 18:55:26 +0200

    add email+pw of paid account

--- main.py
+++ main.py
@@ -5,41 +5,119 @@ import random
 import json
 import logging
 import sys
-from user_agent import generate_user_agent
 
+
+PASSWORDFILE = 'email_pw.txt'
+# first line: email, second: password
+
+
+# change to your needs
+USERAGENT = 'Mozilla/5.0 (X11; Linux x86_64)' + \
+            'AppleWebKit/537.36 (KHTML, like Gecko)' + \
+            'Chrome/87.0.4280.141 Safari/537.36'
 
 logger = logging.getLogger()
-
 
 
 class DeepLQuery:
 
 
-    def __init__(self):
+    def __init__(self, email, password):
 
-        self.id = 1000 * int(random.random()*1000) + 1
+        self.email = email
+        self.password = password
+
+        self.id = self._create_id()
         self.session = requests.Session()
-        self.session.headers['User-Agent'] = generate_user_agent()
+        self.session.headers['User-Agent'] = USERAGENT
 
         r = self.session.get("https://www.deepl.com/translator")
         if r.status_code != 200:
             logger.error('error calling deepl.com')
             return
-        r = self.session.get("https://static.deepl.com/css/deepl.$b89835.css", headers={'referer': "https://www.deepl.com/translator"})
+        r = self.session.get("https://static.deepl.com/css/deepl.$0ab019.css",
+                                headers={'referer': "https://www.deepl.com/translator"})
         if r.status_code != 200:
             logger.error('error getting css 1')
             return
-        r = self.session.get("https://static.deepl.com/css/cookieBanner.$21aa7c.css", headers={'referer': "https://www.deepl.com/translator"})
-        if r.status_code != 200:
-            logger.error('error getting css 2')
-            return
+        #r = self.session.get("https://static.deepl.com/css/cookieBanner.$21aa7c.css",
+                                #headers={'referer': "https://www.deepl.com/translator"})
+        #if r.status_code != 200:
+            #logger.error('error getting css 2')
+            #return
         if not self.session.cookies.keys():
             logger.error('didnt get cookie')
             return
 
+        logger.info('waiting 2sec before login')
+        time.sleep(2)
+
+        r = self.session.post('https://www.deepl.com/PHP/backend/account.php?request_type=jsonrpc&il=EN',
+                                json={"jsonrpc": "2.0",
+                                      "method": "login42",
+                                      "params": {
+                                            "email": self.email,
+                                            "password": self.password,
+                                            "keepLogin": True
+                                      },
+                                      "id": self.id
+                                },
+                                headers={'referer': "https://www.deepl.com/translator"})
+        if r.status_code != 200:
+            logger.error('login returned error')
+            return
+
+        self.id += 1
+        # clientState request
+
+        self.id = self._create_id()
+        r = self.session.post('https://www.deepl.com/PHP/backend/account.php?request_type=jsonrpc&il=EN',
+                                json={"jsonrpc": "2.0",
+                                      "method": "getUserDisplayName",
+                                      "params": {},
+                                      "id": self.id
+                                },
+                                headers={'referer': "https://www.deepl.com/translator"})
+        if r.status_code != 200:
+            logger.error('login +1 returned error')
+            return
+
+        self.id += 1
+        r = self.session.post('https://www.deepl.com/PHP/backend/account.php?request_type=jsonrpc&il=EN',
+                                json={"jsonrpc": "2.0",
+                                      "method": "getActiveSubscriptionInfo",
+                                      "params": {},
+                                      "id": self.id
+                                },
+                                headers={'referer': "https://www.deepl.com/translator"})
+        if r.status_code != 200:
+            logger.error('login +2 returned error')
+            return
+
+        self.id += 1
+        # clientState request
+
+        self.id += 1
+        r = self.session.post('https://www.deepl.com/PHP/backend/account.php?request_type=jsonrpc&il=EN',
+                                json={"jsonrpc": "2.0",
+                                      "method": "getActiveSubscriptionInfo",
+                                      "params": {},
+                                      "id": self.id
+                                },
+                                headers={'referer': "https://www.deepl.com/translator"})
+        if r.status_code != 200:
+            logger.error('login +3 returned error')
+            return
+
+        logger.info('login seemed to work')
+        # fresh id for queries
+        #self.id = self._create_id()
+
+
+    def _create_id(self):
+        return 1000 * int(random.random()*1000) + 2
 
     def query(self, q):
-
         self.id += 1
         timestamp = int(time.time()*100)
 
@@ -58,16 +136,18 @@ class DeepLQuery:
                 "lang": {
                     "user_preferred_langs": ["DE","EN"],
                     "source_lang_user_selected": "auto",
-                    "target_lang": "DE"
+                    "target_lang": "EN"
                 },
-                "priority": -1,
+                "priority": 1,
                 "commonJobParams": {},
                 "timestamp": timestamp
             },
             "id": self.id
         }
 
-        r = self.session.post("https://www2.deepl.com/jsonrpc", json=jj)
+        r = self.session.post("https://api.deepl.com/jsonrpc",
+                                json=jj,
+                                headers={'referer': "https://www.deepl.com/"})
         if r.status_code != 200:
             logger.error('error getting main query')
             logger.error(f'r.status_code: {r.status_code}')
@@ -81,12 +161,32 @@ class DeepLQuery:
             return beam['postprocessed_sentence']
 
 
+    def logout(self):
+        self.id += 1
+        r = self.session.post('https://www.deepl.com/PHP/backend/account.php?request_type=jsonrpc&il=EN',
+                                json={"jsonrpc": "2.0",
+                                      "method": "logout",
+                                      "params": {},
+                                      "id": self.id
+                                },
+                                headers={'referer': "https://www.deepl.com/translator"})
+        if r.status_code != 200:
+            logger.error('logout returned error')
+            return
+
 
 if __name__ == '__main__':
 
-    dlq = DeepLQuery()
+    with open(PASSWORDFILE) as pwfile:
+        email, password = pwfile.read().strip().split('\n')
+
+    dlq = DeepLQuery(email, password)
 
     while True:
+        print('Type query:')
         inp = sys.stdin.readline().strip()
+        if inp == 'q':
+            dlq.logout()
+            break
         print(dlq.query(inp))
 
